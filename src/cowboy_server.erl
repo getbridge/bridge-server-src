ad1183a9112b64cc4be2c119b035cb0fc8027c2e
ericz <really.ez@gmail.com>
2012-04-01 01:35:13 -0700

    noop logger

--- src/cowboy_server.erl
+++ src/cowboy_server.erl
@@ -15,7 +15,7 @@ init()->
   
   application:start(cowboy),
        
-      GatewaySockJs = sockjs_handler:init_state(<<"/bridge">>, fun gateway_sockjs:handle_sockjs/2, [{logger, fun(_,_) -> ok end}]),
+      GatewaySockJs = sockjs_handler:init_state(<<"/bridge">>, fun gateway_sockjs:handle_sockjs/2, [{logger, fun(_,Req,_) -> Req end}]),
 
        
        Dispatch = [{'_', % domain specifier
